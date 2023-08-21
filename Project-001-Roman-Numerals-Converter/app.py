a638193cea2ae0f845dc105ffea3a35f3da75931
MrBaykal <ebaykal2266@gmail.com>
2023-08-21 12:27:50 +0200

    App.py and YAML are modified

--- Project-001-Roman-Numerals-Converter/app.py
+++ Project-001-Roman-Numerals-Converter/app.py
@@ -21,7 +21,7 @@ def main_post():
         number = int(alpha)
         if not 0 < number < 4000:
             return render_template('index.html', developer_name='Emre', not_valid=True)
-        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Name')
+        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Emre')
     else:
         return render_template('index.html', developer_name='Emre', not_valid=False)
 
--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
@@ -15,13 +15,13 @@ def head():
 # and assign to the route of ('/')
 @app.route("/list")
 def header():
-    names =["Casper", "Sam", "Jasper"]
-    # numbers = range (1,11)
-    return render_template("body.html", object = names)
+    # names =["Casper", "Sam", "Jasper"]
+    numbers = range (1,11)
+    return render_template("body.html", object = numbers)
 
 
 # run this app in debug mode on your local.
 if __name__== "__main__":
-    # app.run(debug=True, port=3000)
+    # app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
 
